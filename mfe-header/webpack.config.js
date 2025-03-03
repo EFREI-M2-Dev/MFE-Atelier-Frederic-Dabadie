bf41a2462b402dc157e820bd3759717677cefa93
Fredray-21 <dabadie.frederic@gmail.com>
2025-03-03 12:32:34 +0100

    init(mfe-header) init webpack config

---
+++ mfe-header/webpack.config.js
@@ -0,0 +1,50 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { ModuleFederationPlugin } from '@module-federation/enhanced/webpack';
+
+export default {
+    entry: './src/index.js',
+    mode: 'development',
+    devServer: {
+        port: 3001,
+    },
+    output: {
+        publicPath: 'http://localhost:3001/',
+    },
+    cache: false,
+    resolve: {
+        extensions: ['.jsx', '.js'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+        }),
+        new ModuleFederationPlugin({
+            name: 'mfe_header',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './Header': './src/components/Header.jsx',
+            },
+            shared: {
+                react: {
+                    singleton: true,
+                    requiredVersion: '^18.2.0',
+                },
+                'react-dom': {
+                    singleton: true,
+                    requiredVersion: '^18.2.0',
+                },
+            },
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+        ],
+    },
+};
