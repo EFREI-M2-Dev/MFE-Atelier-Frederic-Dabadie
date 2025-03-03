719327702353ee72ebb18e5227c7bc4b8fe396f1
Fredray-21 <dabadie.frederic@gmail.com>
2025-03-03 12:33:07 +0100

    feat(mfe_header) component Header react

---
+++ mfe-header/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {Header} from './components/Header.jsx';
+
+const root = document.getElementById('root');
+ReactDOM.createRoot(root).render(<Header />);
